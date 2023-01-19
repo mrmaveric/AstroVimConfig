13b5263af3c56948ad1dc1bfb36f5111b7e16af2
James Bull <contact@jamesbull.me>
2023-01-19 23:27:02 +1100

    Adding colourscheme with transparency
    I've added my previous colourscheme tokyonight with a transparency
    option so I can feel super cool.
    
    I've also changed my terminal exit insert mode shortcut from jjj to
    ppp because this git popup UI counts as terminal mode and I can't move
    around it fast with the J key.

--- init.lua
+++ init.lua
@@ -26,7 +26,8 @@ local config = {
         },
 
         -- Set colorscheme to use
-        colorscheme = "default_theme",
+        -- colorscheme = "default_theme",
+        colorscheme = "tokyonight",
 
         -- Add highlight groups in any theme
         highlights = {
@@ -218,7 +219,7 @@ local config = {
                         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
                 },
                 t = {
-                        ["jjj"] = {"<C-\\><C-n>", desc = "leave insert mode in terminal"},
+                        ["ppp"] = {"<C-\\><C-n>", desc = "leave insert mode in terminal"},
                         -- setting a mapping to false will disable it
                         -- ["<esc>"] = false,
                 },
@@ -233,6 +234,36 @@ local config = {
         -- Configure plugins
         plugins = {
                 init = {
+                        {
+                                "folke/tokyonight.nvim",
+                                as = "tokyonight",
+                                config = function()
+                                        require("tokyonight").setup({
+                                                -- your configuration comes here
+                                                -- or leave it empty to use the default settings
+                                                style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+                                                light_style = "day", -- The theme is used when the background is set to light
+                                                transparent = true, -- Enable this to disable setting the background color
+                                                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+                                                styles = {
+                                                        -- Style to be applied to different syntax groups
+                                                        -- Value is any valid attr-list value for `:help nvim_set_hl`
+                                                        comments = { italic = true },
+                                                        keywords = { italic = true },
+                                                        functions = {},
+                                                        variables = {},
+                                                        -- Background styles. Can be "dark", "transparent" or "normal"
+                                                        sidebars = "dark", -- style for sidebars, see below
+                                                        floats = "dark", -- style for floating windows
+                                                },
+                                                sidebars = { "qf", "help" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+                                                day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+                                                hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+                                                dim_inactive = false, -- dims inactive windows
+                                                lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+                                        })
+                                end,
+                        },
                         -- You can disable default plugins as follows:
                         -- ["goolord/alpha-nvim"] = { disable = true },
 
